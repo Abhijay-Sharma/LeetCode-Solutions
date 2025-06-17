fd881fe407abfa2700b29ecd97e0df06cb38e614
Abhijay Sharma <159028823+Abhijay-Sharma@users.noreply.github.com>
2025-06-17 17:13:25 +0000

    [LeetCode Sync] Runtime - 8 ms (2.58%), Memory - 12.9 MB (30.36%)

--- leetcode_solutions/0081-search-in-rotated-sorted-array-ii/solution.py
+++ leetcode_solutions/0081-search-in-rotated-sorted-array-ii/solution.py
@@ -5,27 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        length=len(nums)
-        start=0
-        end=length-1
-        while start <= end and start<length and end<length:
+        start,end=0,len(nums)-1
+
+        while start<=end:
             mid=(start+end)//2
+            print(start,mid,end)
             if nums[mid]==target:
                 return True
-            #Edge case
-            if nums[start]==nums[mid] and nums[mid] == nums [end]:
+            if nums[start]==nums[mid] and nums[mid]==nums[end]:     # This handles the cases of repetion
                 start+=1
                 end-=1
                 continue
-            #If left part is sorted
-            if nums[start]<=nums[mid]:
-                if nums[start]<= target<=nums[mid]:#element exists here
+            if nums[start]<=nums[mid] :   # if left half is sorted
+                if nums[start]<=target<=nums[mid]:  # if target is in left sorted half
                     end=mid-1
                 else:
                     start=mid+1
-            # If right part is sorted
-            elif nums[mid]<=nums[end]:
-                if  nums[mid]<=target<=nums[end]: #element exists here
+            elif nums[end]>=nums[mid]:     # if right half is sorted
+                if nums[mid]<=target<=nums[end]:    # if target is in right sorted half
                     start=mid+1
                 else:
                     end=mid-1
