a248ca1a690dbe4d8dd589a8353c4f172329bd78
Abhijay Sharma <159028823+Abhijay-Sharma@users.noreply.github.com>
2025-06-19 15:41:54 +0000

    [LeetCode Sync] Runtime - 3 ms (26.77%), Memory - 16.5 MB (37.01%)

--- leetcode_solutions/0540-single-element-in-a-sorted-array/solution.py
+++ leetcode_solutions/0540-single-element-in-a-sorted-array/solution.py
@@ -4,27 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        length=len(nums)
-        start=0
-        end=length-1
-        # Edge cases:
-        if length == 1:
-            return nums[0]
-        if nums[0] != nums[1]:
-            return nums[0]
-        if nums[length - 1] != nums[length - 2]:
-            return nums[length - 1]
-        while start <= end:
+        start,end=0,len(nums)-1
+
+        while start<end:
             mid=(start+end)//2
-            if nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]:
-                return nums[mid]
+
+            #first step is to identify odd and even halves
+
+            if mid%2==1:
+                mid-=1
+
             if nums[mid]==nums[mid+1]:
-                if (end-mid-1)%2!=0:
-                    start=mid+2
-                else:
-                    end=mid-1
-            if nums[mid]==nums[mid-1]:
-                if (mid-start-1)%2!=0:
-                    end=mid-2
-                else:
-                    start=mid+1
+                start+=2
+            else:
+                end=mid
+
+        return nums[start]
